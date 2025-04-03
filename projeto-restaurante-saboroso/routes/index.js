2a6b4b9b52170657f8730bd2329f65ac0981ea8b
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-03 11:30:33 -0300

    chore: Change the html files to ejs. feat: Added view and started working with routes and dynamic content with includes.

--- projeto-restaurante-saboroso/routes/index.js
+++ projeto-restaurante-saboroso/routes/index.js
@@ -1,9 +1,62 @@
+var con = require("./../inc/db");
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+   con.query(
+    `SELECT * FROM tb_menus ORDER BY title `
+   ,(err, results)=>{
+
+      if(err){
+        console.log(err);
+        return res.status(500).send("Erro no banco de dados.");
+      }
+
+      res.render('index', {
+        title: 'Restaurante Saboroso!',
+        menus: results
+
+      });
+
+   });
+  
 });
+
+
+router.get('/contacts', function(req, res, next) {
+    res.render('contacts',{
+      title: 'Contato - Restaurante Saboroso!',
+      background: 'images/img_bg_3.jpg',
+      h1: 'Diga oi!'
+    });
+});
+
+router.get('/menus', function(req, res, next) {
+    res.render('menus',{
+      title: 'Menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!'
+    });
+});
+
+router.get('/reservations', function(req, res, next) {
+    res.render('reservations',{
+      title: 'Reservations - Restaurante Saboroso!',
+      background: 'images/img_bg_2.jpg',
+      h1: 'Reserve uma Mesa!'
+    });
+});
+
+router.get('/services', function(req, res, next) {
+    res.render('services',{
+      title: 'Services - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'É um prazer poder servir!'
+    });
+});
+
+
 
 module.exports = router;
