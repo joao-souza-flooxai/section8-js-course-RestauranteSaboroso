309b7964cf6cdbc62b2bd7379c8eec51a88f7bad
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-04 11:18:28 -0300

    refactor: Refactoring admin page, using includes. feat: Added admin menu navigation.

---
+++ projeto-restaurante-saboroso/inc/admin.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+    getMenus() {
+        return [
+            {
+                text: "Tela Inicial",
+                href: "/admin",
+                gon: true,
+                active: false
+            },
+            {
+                text: "Menu",
+                href: "/admin/menus",
+                icon: "cutlery",
+                active: false
+            },
+            {
+                text: "Reservas",
+                href: "/admin/reservations",
+                icon: "gel",
+                active: false
+            },
+            {
+                text: "Contatos",
+                href: "/admin/contacts",
+                icon: "comments",
+                active: false
+            },
+            {
+                text: "usuarios",
+                href: "/admin/users",
+                icon: "users",
+                active: false
+            },
+            {
+                text: "E-mails",
+                href: "/admin/emails",
+                icon: "envelope",
+                active: false
+            }
+
+        ];
+    }
+}
--- projeto-restaurante-saboroso/routes/admin.js
+++ projeto-restaurante-saboroso/routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var users = require("../inc/users");
+var admin = require("./../inc/admin");
 
 router.use(function(req, res, next) {
     if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
@@ -8,6 +9,11 @@ router.use(function(req, res, next) {
     } else {
         next();
     }
+});
+
+router.use(function(req, res, next) {
+    req.menus = admin.getMenus();
+    next();
 });
 
 router.get("/logout", function(req, res, next) {
@@ -18,7 +24,7 @@ router.get("/logout", function(req, res, next) {
 
 router.get("/", function(req, res, next) {
     res.render("admin/index", {
-        
+        menus: req.menus
     });
 });
 
@@ -49,25 +55,34 @@ router.get("/login", function(req, res, next) {
 });
 
 router.get("/contacts", function (req, res, next) {
-    res.render("admin/contacts");
+    res.render("admin/contacts", {
+        menus: req.menus
+    });
 });
 
 router.get("/emails", function (req, res, next) {
-    res.render("admin/emails");
+    res.render("admin/emails", {
+        menus: req.menus
+    });
 });
 
 router.get("/menus", function (req, res, next) {
-    res.render("admin/menus");
+    res.render("admin/menus", {
+        menus: req.menus
+    });
 });
 
 router.get("/reservations", function (req, res, next) {
     res.render("admin/reservations",{
-        date: {}
+        date: {},
+        menus: req.menus
     });
 });
 
 router.get("/users", function (req, res, next) {
-    res.render("admin/users");
+    res.render("admin/users", {
+        menus: req.menus
+    });
 });
 
 
