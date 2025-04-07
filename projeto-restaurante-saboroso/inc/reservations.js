64cc40f9b2e3c5a61a40d9bdcab994926b23703d
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-07 10:34:13 -0300

    feat: Added reservation read.

--- projeto-restaurante-saboroso/inc/reservations.js
+++ projeto-restaurante-saboroso/inc/reservations.js
@@ -12,6 +12,17 @@ module.exports = {
         });
     },
 
+    getReservations() {
+        return new Promise((resolve, reject) => {
+            conn.query("SELECT * FROM tb_reservations ORDER BY date DESC", (err, results) => {
+                
+                if (err) reject(err);
+                else resolve(results);
+
+            });
+        });
+    },
+
     save(fields){
 
         if (fields.date.indexOf('/') > -1) {
