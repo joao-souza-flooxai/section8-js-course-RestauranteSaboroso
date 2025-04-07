366ba2c909cf63b06e79dbafea589d7ed95973f0
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-07 10:45:41 -0300

    feat: Added date format on reservations using moment.js. chore: Added moment dependency.

--- projeto-restaurante-saboroso/routes/admin.js
+++ projeto-restaurante-saboroso/routes/admin.js
@@ -4,6 +4,9 @@ var users = require("../inc/users");
 var admin = require("./../inc/admin");
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
+var moment = require('moment');
+moment.locale("pt-BR");
+
 router.use(function(req, res, next) {
     if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
         res.redirect("/admin/login");
@@ -112,7 +115,8 @@ router.get("/reservations", function (req, res, next) {
     reservations.getReservations().then(data => {
         res.render("admin/reservations", admin.getParams(req, {
             date: {},
-            data
+            data, 
+            moment
         }));
     });
 });
